1a59f3e1d06ff5a7a05d019efda568c29a20a75e
jurin-n <jurin.nakano@gmail.com>
2019-03-02 10:32:34 +0900

    CBOWモデルとこのモデル使った学習コードを書いてみた。

--- _books/deep-learning-from-scratch-2/common/layers.py
+++ _books/deep-learning-from-scratch-2/common/layers.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 class MatMul:
     def __init__(self, W):
@@ -18,4 +19,31 @@ class MatMul:
         dx = np.dot(dout, W.T)
         dW = np.dot(self.x.T, dout)
         self.grads[0][...] = dW
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師ラベル
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 教師ラベルがone-hotベクトルの場合、正解のインデックスに変換
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
         return dx
