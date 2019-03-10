d3c77ebc1036f41f22fc4b120c4f9fff9272a16a
jurin-n <jurin.nakano@gmail.com>
2019-03-10 13:56:18 +0900

    Jupyter Notebook起動スクリプトを追加。

--- _books/deep-learning-from-scratch-2/common/layers.py
+++ _books/deep-learning-from-scratch-2/common/layers.py
@@ -64,3 +64,27 @@ class Embedding:
         dW, = self.grads
         dW[...] = 0
         np.add.at(dW, self.idx, dout)
+
+
+class EmbeddingDot:
+    def __init_(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out
+
+    def backword(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
