3a3988880fd6cb0a099d408b949925a701c07946
jurin-n <jurin.nakano@gmail.com>
2019-03-23 09:42:36 +0900

    Mediumに乗ってたchars2vecコード動かしてみた。

--- _books/deep-learning-from-scratch-2/common/layers.py
+++ _books/deep-learning-from-scratch-2/common/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
 from common.functions import softmax, cross_entropy_error
-
+from samplers import UnigramSampler
 
 class MatMul:
     def __init__(self, W):
@@ -115,7 +115,7 @@ class EmbeddingDot:
         return dh
 
 
-def NegativeSamplingLoss:
+class NegativeSamplingLoss:
     def __init__(self, W, corpus, power=0.75, sample_size=5):
         self.sample_size = sample_size
         self.sampler = UnigramSampler(corpus, power, sample_size)
@@ -126,3 +126,29 @@ def NegativeSamplingLoss:
         for layer in self.embed_dot_layers:
             self.params += layer.params
             self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        # 正例のフォワード
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        # 負例のフォワード
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1 + i].forward(h, negative_target)
+            loss += self.loss_layers[1 + i].forward(score, negative_label)
+      
+        return loss
+
+    def backward(self, dout=1):
+        dh = 0
+        for l0, l1 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+
+        return dh
